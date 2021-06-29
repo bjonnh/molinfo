cdafbe5b8438cb363fd7f2546e6ed91fc86e8102
Jonathan Bisson <bjonnh-code@bjonnh.net>
2021-06-29 12:56:29 -0500

    Add simple test.

--- src/test/kotlin/net/nprod/skotliton/mainTest.kt
+++ src/test/kotlin/net/nprod/skotliton/mainTest.kt
@@ -1,10 +1,18 @@
 package net.nprod.skotliton
 
+import net.nprod.molinfo.chemistry.MoleculeManager
 import org.junit.jupiter.api.Test
 
 internal class mainTest {
     @Test
     fun `test Main ok`() {
         assert(true)
+    }
+
+    @Test
+    fun `check canonical smiles support`() {
+        val mm = MoleculeManager()
+        val mol = mm.moleculeFromSmiles("OCC1C2C(=CN3CCC4(C=5C=CC=CC5NC14)C3C2)C(C6=CC=C7NC8C(CO)C9CC%10N(CCC8%10C7=C6)CC9C=C)C")
+        println(mol.svg())
     }
 }
